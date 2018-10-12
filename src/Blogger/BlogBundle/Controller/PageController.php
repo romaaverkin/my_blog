df9568a048d43a84ac862b696e994a1ee6cab52a
Roman Averkin <romaaverkin@gmail.com>
2018-10-12 09:51:06 +0700

    Создал главную страницу BloggerBlogBundle

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -4,10 +4,10 @@ namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class PageController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('@BloggerBlog/Default/index.html.twig');
+        return $this->render('@BloggerBlog/Page/index.html.twig');
     }
 }
