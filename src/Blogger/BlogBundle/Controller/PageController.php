37f0dbfa40908a6e1fc0ef2a4ede6d7e6f5498fd
Roman <romaaverkin@gmail.com>
2018-10-16 11:23:27 +0700

    Создал форму для контактов

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -2,7 +2,10 @@
 
 namespace Blogger\BlogBundle\Controller;
 
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends Controller
@@ -26,9 +29,30 @@ class PageController extends Controller
 
     /**
      * @Route("/contact", name="contact")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return $this->render('@BloggerBlog/Page/contact.html.twig');
+        $enquiry = new Enquiry();
+
+        $form = $this->createForm(EnquiryType::class, $enquiry);
+
+        if ($request->isMethod($request::METHOD_POST)) {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                // Perform some action, such as sending an email
+
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('contact'));
+            }
+        }
+
+        return $this->render('@BloggerBlog/Page/contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
+
 }
