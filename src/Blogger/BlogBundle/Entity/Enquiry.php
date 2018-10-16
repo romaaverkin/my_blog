cce2e55278cff55b07a99b84b316b066883e204f
Roman <romaaverkin@gmail.com>
2018-10-16 11:57:41 +0700

    Добавил валидацию в контактах

--- src/Blogger/BlogBundle/Entity/Enquiry.php
+++ src/Blogger/BlogBundle/Entity/Enquiry.php
@@ -3,8 +3,26 @@
 namespace Blogger\BlogBundle\Entity;
 
 
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+
 class Enquiry
 {
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('name', new NotBlank());
+        $metadata->addPropertyConstraint('email', new Email());
+        $metadata->addPropertyConstraint('subject', new Length(array(
+            'max' => 50
+
+        )));
+        $metadata->addPropertyConstraint('body', new Length(array(
+            'min' => 50
+        )));
+    }
+
     protected $name;
 
     protected $email;
